f114bc0cbf8a1f37bcd321ad57af729ddadaf2cb
Paweł Węgiel <pawel.wegiel@cloudware.pl>
2022-11-02 12:50:04 +0100

    added another fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,38 @@
 import "./App.css";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const data = {
   url: process.env.REACT_APP_URL as string,
   userName: process.env.REACT_APP_USER_NAME as string,
   password: process.env.REACT_APP_PASSWORD as string,
+  ttUrl: process.env.REACT_APP_TT_URL as string,
 };
 
 function App() {
+  const [token, setToken] = useState("");
   useEffect(() => {
     const { url, userName, password } = data;
     axios
       .post(url, { user_name: userName, password: password })
-      .then((response) => console.log(response.data))
+      .then((response) => setToken(response.data.access))
       .catch((error) => console.log(error));
   }, []);
+
+  useEffect(() => {
+    if (token !== "") {
+      const { ttUrl } = data;
+      axios
+        .get(ttUrl, {
+          headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => console.log(response.data))
+        .catch((error) => console.log(error));
+    }
+  }, [token]);
 
   return (
     <div className="App">
